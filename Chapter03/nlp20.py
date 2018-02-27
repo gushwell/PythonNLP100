f33f8f0a84cd568f1503ca5de93983d2e46aa5a2
gushwell <gushwell@gmail.com>
2018-02-27 21:37:30 +0900

    add No.20

---
+++ Chapter03/nlp20.py
@@ -0,0 +1,18 @@
+import json
+import gzip
+
+def extract(title):
+    with gzip.open('jawiki-country.json.gz', 'rt', encoding='utf8') as fin:
+        for line in fin:
+            jsd = json.loads(line)
+            if jsd['title'] == title:
+                return jsd['text']
+    return ''
+
+def main():
+    article = extract('イギリス')
+    with open('england-article.txt', 'w', encoding='utf8') as fout:
+        fout.write(article)
+
+if __name__ == '__main__':
+    main()
