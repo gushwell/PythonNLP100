5c42ccc38d879c97e2fedebe4133017d02f9e612
gushwell <gushwell@gmail.com>
2018-03-24 20:30:54 +0900

    add No.30

---
+++ Chapter04/nlp30.py
@@ -0,0 +1,31 @@
+import re
+
+def analyze():
+    lines = []
+    sentence = []
+    with open('neko.txt.mecab', 'r', encoding='utf8') as fin:
+        for line in fin:
+            words = re.split(r'\t|,|\n', line)
+            if words[0] == 'EOS':
+                if sentence:
+                    lines.append(sentence)
+                    sentence = []
+                continue
+            sentence.append({
+                "surface": words[0],
+                "base": words[7],
+                "pos": words[1],
+                "pos1": words[2],
+            })
+    return lines
+
+def main():
+    article = analyze()
+    print(article[0])
+    print()
+    print(article[1])
+    print()
+    print(article[2])
+
+if __name__ == '__main__':
+    main()
