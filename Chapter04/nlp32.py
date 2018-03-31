6cb7175d170775c1f3c97d6fbca57bb85e519205
gushwell <gushwell@gmail.com>
2018-03-31 10:05:08 +0900

    add No.32

---
+++ Chapter04/nlp32.py
@@ -0,0 +1,33 @@
+import re
+ 
+def analyze():
+    lines = []
+    sentence = []
+    with open('neko.txt.mecab', 'r', encoding='utf8') as fin:
+        for line in fin:
+            words = re.split(r'\t|,|\n', line)
+            if words[0] == 'EOS':
+                if sentence:
+                    lines.append(sentence)
+                    sentence = []
+                continue
+            sentence.append({
+                "surface": words[0],
+                "base": words[7],
+                "pos": words[1],
+                "pos1": words[2],
+            })
+    return lines
+
+def extractVerbs(lines):
+    for sentense in lines:
+        yield from map(lambda w: (w['surface'], w['base']), \
+            filter(lambda w: w['pos'] == '動詞', sentense))
+
+def main():
+    article = analyze()
+    verbs = [(surface, base) for surface, base in extractVerbs(article)]
+    print(verbs[:50])
+
+if __name__ == '__main__':
+    main()
